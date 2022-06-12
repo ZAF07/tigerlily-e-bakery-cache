e814aa7e2450e308fa5627921d574a176c4e992c
ZAF07 <zaffere07@gmail.com>
2022-06-12 23:40:20 +0800

    initial commit

---
+++ gen_rpc.sh
@@ -0,0 +1,19 @@
+echo 'export GOPATH=$HOME/Go' >> $HOME/.bashrc source $HOME/.bashrc
+
+# rm -rf protos/rpc/*
+
+# protoc --go_out=. --go_opt=paths=source_relative ./protos/*
+protoc --go_out=rpc --go_opt=paths=source_relative ./proto/inventory/inventory.proto
+
+# --go-grpc_out=protos  --go-grpc_opt=paths=source_relative ./proto/inventory/*
+
+echo "Copying into protos.."
+cp -R rpc/proto/inventory/* rpc
+
+echo "Cleaning protos directory.."
+rm -rf rpc/proto
+
+# echo "Commtting code so that cahnges will reflect.."
+# git commit -am 'Updating submodules'
+
+echo "DONE"
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ZAF07/tigerlily-e-bakery-cache
+
+go 1.17
+
+require google.golang.org/protobuf v1.28.0
